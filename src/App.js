23005c5ae72b4c6f6aea4a702c72234411c85a16
GeneralDeD <sh.fayztex@mail.ru>
2020-10-12 16:00:45 +0500

    menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import './component/menu/menu'
 
 function App() {
   return (
